110f424588f8399aa755d7461b29e7b847735099
Evgeniy Morozov <morozov.evgeniy.95@gmail.com>
2021-09-05 23:25:02 +0300

    Extract operators from string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the equation:");
+
+    let mut eq = String::new();
+
+    io::stdin()
+        .read_line(&mut eq)
+        .expect("Failed to read the equation");
+
+    let mut operators: Vec<char> = Vec::new();
+    for c in eq.chars() {
+        match c {
+            '+' | '-' | '*' | '/' => operators.push(c),
+            _ => {}
+        }
+    }
+
+    println!("Discovered operators (in reverse order):");
+    while let Some(op) = operators.pop() {
+        println!("Discovered operator: {}", op);
+    }
 }
