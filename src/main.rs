4417aed6bd5626567291f913c7b6e54f1288eec5
Evgeniy Morozov <morozov.evgeniy.95@gmail.com>
2021-09-07 00:33:06 +0300

    Add basic stack machine for evaluation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-    println!("Enter the equation:");
+    println!("Enter the equation (in RPN):");
 
     let mut eq = String::new();
 
@@ -9,16 +9,48 @@ fn main() {
         .read_line(&mut eq)
         .expect("Failed to read the equation");
 
-    let mut operators: Vec<char> = Vec::new();
-    for c in eq.chars() {
-        match c {
-            '+' | '-' | '*' | '/' => operators.push(c),
-            _ => {}
+    let mut stack: Vec<f64> = Vec::new();
+
+    // TODO: parse the RPN char-by-char
+    for word in eq.split_whitespace() {
+        match word {
+            // If we encountered an operator -- try to evaluate what we have currently on stack
+            "+" | "-" | "*" | "/" => {
+                if stack.len() < 2 {
+                    println!(
+                        "Not enough operands for operator '{}' (got {}, expected 2)",
+                        word,
+                        stack.len()
+                    );
+                    break;
+                }
+                let op1 = stack.pop().unwrap();
+                let op2 = stack.pop().unwrap();
+
+                match word {
+                    "+" => stack.push(op1 + op2),
+                    "-" => stack.push(op1 - op2),
+                    "*" => stack.push(op1 * op2),
+                    "/" => stack.push(op1 / op2),
+                    _ => panic!("Unexpected operator {}", word),
+                }
+            }
+            // Otherwise try to parse the number and error out if parsing fails
+            _ => {
+                let f = word.parse::<f64>();
+                if f.is_err() {
+                    println!("Value {} is NaN", word);
+                    break;
+                }
+                let num = f.unwrap();
+                stack.push(num);
+            }
         }
     }
 
-    println!("Discovered operators (in reverse order):");
-    while let Some(op) = operators.pop() {
-        println!("Discovered operator: {}", op);
+    if stack.len() != 1 {
+        println!("The equation is incomplete, evaluation aborted");
+        return;
     }
+    println!("Result = {}", stack.pop().unwrap());
 }
